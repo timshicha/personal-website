c434b7a6ba9f2d0a8d2ff6c2d1bf0dfcb4722efa
Timofey Shichalin <76839122+timshicha@users.noreply.github.com>
2023-07-15 23:30:58 -0700

    added main.tsx, can now run site in dev mode

---
+++ frontend/src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const rootContainer: HTMLElement = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(rootContainer).render(
+    <React.StrictMode>
+        
+    </React.StrictMode>
+)
