ce34472e808ef4f37167326125db23e1aac7d0cf
Timofey Shichalin <76839122+timshicha@users.noreply.github.com>
2023-07-15 23:39:31 -0700

    Added routes (home page)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const rootContainer: HTMLElement = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(rootContainer).render(
     <React.StrictMode>
-        
+        <App />
     </React.StrictMode>
 )
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Home() {
+    return (
+        <div>
+            <p>
+                This is the home page.
+            </p>
+        </div>
+    );
+}
+
+export default Home;
