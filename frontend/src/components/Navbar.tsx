d6f82c47e02a2ff9b5d4afbd0ed427499d0b15a6
Timofey Shichalin <76839122+timshicha@users.noreply.github.com>
2023-07-16 23:47:56 -0700

    copied navbar from prev project

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import HomeImg from '../assets/home-img.png';
+import HamburgerMenuImg from '../assets/hamburger-menu.png';
+import '../styles/Navbar.css';
+
+const Navbar = () => {
+  const [menuExpanded, setMenuExpanded] = useState(false);
+
+  return (
+    <nav className="nav-bar">
+      <Link to="/" className="home-button">
+        <img src={HomeImg} alt="Go to home page" className="home-icon" />
+      </Link>
+      <div className="nav-table">
+        <div className="nav-item">
+          <Link to="/" className="nav-link about-me-link">
+            About Me
+          </Link>
+        </div>
+        <div className="nav-item">
+          <Link to="/previous-work" className="nav-link previous-work-link">
+            Previous Work
+          </Link>
+        </div>
+        <div className="nav-item">
+          <Link to="/projects" className="nav-link projects-link">
+            Projects
+          </Link>
+        </div>
+        <div className="nav-item">
+          <Link to="/contact" className="nav-link contact-link">
+            Contact
+          </Link>
+        </div>
+      </div>
+
+      <div className="menu-div">
+        <button
+          type="button"
+          className="nav-menu-btn"
+          onClick={() => setMenuExpanded(!menuExpanded)}
+        >
+          <img
+            src={HamburgerMenuImg}
+            alt="Expand the navigation bar"
+            className="menu-icon"
+          />
+        </button>
+        <ul className={'expanded-menu ' + (menuExpanded ? '' : 'hidden')}>
+          <li className="menu-item">
+            <Link to="/" className="menu-link">
+              About Me
+            </Link>
+          </li>
+          <li className="menu-item">
+            <Link to="/previous-work" className="menu-link">
+              Previous Work
+            </Link>
+          </li>
+          <li className="menu-item">
+            <Link to="/projects" className="menu-link">
+              Projects
+            </Link>
+          </li>
+          <li className="menu-item">
+            <Link to="/contact" className="menu-link">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 export function Home() {
     return (
         <div>
+            <Navbar />
             <p>
                 This is the home page.
             </p>
