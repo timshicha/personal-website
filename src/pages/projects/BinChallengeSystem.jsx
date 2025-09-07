9cbc672ebb0996263071d8947d18b3ebc7cfb314
Tim Shichalin <shichatim@gmail.com>
2025-09-06 23:57:34 -0700

    bin challenge summary and inspiration

--- src/pages/projects/BinChallengeSystem.jsx
+++ src/pages/projects/BinChallengeSystem.jsx
@@ -13,7 +13,17 @@ const BinChallengeSystemPage = () => {
                 <h1 className="text-[25px] font-bold text-center">Bin Challenge</h1>
                 <h2 className="font-bold text-[18px]">Summary</h2>
                 <p>
-                    
+                    Want to see your arm position displayed on a screen? Attach this system to your
+                    arm and connect to the iPhone app to see the current angle of your arm.
+                </p>
+                <h2 className="font-bold text-[18px] mt-5">Insiration</h2>
+                <p>
+                    I don't like losing. What's worse is when you lose and you know that the other
+                    person cheated. This is what was happening at my job. We had a challenge to see
+                    who could hold a bin with their outstretched arm the longest. But I began to realize
+                    that many people were dropping their arm but still "going". So I created a system
+                    to ensure integrity. Competitors would attach the system to their arm, and then the
+                    timer in the app would run until the arm was dropped below a specified threshold.
                 </p>
             </div>
         </>
