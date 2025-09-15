64dc867e3ef660bf7a6100ad4d818a2d2db7cf7f
Tim Shichalin <shichatim@gmail.com>
2025-09-15 00:10:19 -0700

    added more contact instructions

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,7 +15,18 @@ const ContactPage = () => {
                 Contact
             </div>
             <div className="w-full bg-gray-700 text-white font-georgia text-[20px] p-10">
-                <p className="text-center">To contact, please message me on <a href="https://www.linkedin.com/in/shichatim/" className="underline text-blue-300">LinkedIn</a>.</p>
+                <p className="mb-10">The following are the ways you can get ahold of me:</p>
+                <p className="mb-2 text-blue-300">If you have my resume:</p>
+                <ul className="list-disc ml-8">
+                    <li>Text <span className="text-gray-500">(preferably)</span></li>
+                    <li>Call <span className="text-gray-500">(Please text first; I may not pick up if your number gets flagged)</span></li>
+                    <li>Email <span className="text-gray-500">(may take a few hours)</span></li>
+                    <li>Send me a message via <a href="https://www.linkedin.com/in/shichatim/" className="underline text-blue-300">LinkedIn</a> <span className="text-gray-500">(may take a few hours)</span></li>
+                </ul>
+                <p className="mb-2 mt-10 text-blue-300">If you do NOT have my resume:</p>
+                <ul className="list-disc ml-8">
+                    <li>Send me a message via <a href="https://www.linkedin.com/in/shichatim/" className="underline text-blue-300">LinkedIn</a> <span className="text-gray-500">(may take a few hours)</span></li>
+                </ul>
             </div>
         </div>
     );
